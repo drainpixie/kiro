db862413035c6bd4378cb4b87c8620d53e2e30be
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-03-13 00:00:20 +0100

    refactor(client): 🔨 restructure directory

---
+++ client/src/constants.rs
@@ -0,0 +1,2 @@
+pub const APP_NAME: &str = "Kiro";
+pub const PADDING: f32 = 8.0;
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,114 +1,17 @@
+use constants::*;
+use eframe::{run_native, NativeOptions};
+use egui::Theme;
+use nodes::Node;
+use render::Kiro;
 use std::collections::HashSet;
 
-use eframe::{run_native, App, NativeOptions};
-use egui::{
-    text::LayoutJob, Align, Button, CentralPanel, FontSelection, RichText, ScrollArea, Separator,
-    Theme,
-};
-use rand::Rng;
-
-const APP_NAME: &str = "Kiro";
-const PADDING: f32 = 8.0;
-
-struct Node<'a> {
-    id: i32,
-
-    hostname: &'a str,
-    address: &'a str,
-}
-
-struct Kiro<'a> {
-    nodes: Vec<Node<'a>>,
-    selected: Option<i32>,
-}
+mod constants;
+mod nodes;
+mod render;
 
 // TODO: Custom fonts
 // TODO: In-app config
 // TODO: Better styling
-
-impl<'a> Node<'a> {
-    fn new(ids: &mut HashSet<i32>, hostname: &'a str, address: &'a str) -> Self {
-        let mut rng = rand::rng();
-        let mut id: i32;
-
-        loop {
-            id = rng.random_range(1..=9999);
-            if !ids.contains(&id) {
-                ids.insert(id);
-                break;
-            }
-        }
-
-        Self {
-            id,
-            hostname,
-            address,
-        }
-    }
-}
-
-impl<'a> Kiro<'a> {
-    fn new(nodes: Vec<Node<'a>>) -> Self {
-        Self {
-            nodes,
-            selected: None,
-        }
-    }
-
-    fn render_nodes(&mut self, ui: &mut egui::Ui) {
-        ScrollArea::vertical().show(ui, |ui| {
-            let available_height = ui.max_rect().height();
-
-            ui.horizontal(|ui| {
-                ui.vertical(|ui| {
-                    for node in &self.nodes {
-                        let style = ui.style();
-
-                        let mut layout_job = LayoutJob::default();
-
-                        RichText::new(node.hostname).strong().size(14.0).append_to(
-                            &mut layout_job,
-                            &style,
-                            FontSelection::Default,
-                            Align::LEFT,
-                        );
-
-                        RichText::new(format!("\n{}", node.address))
-                            .size(12.0)
-                            .append_to(
-                                &mut layout_job,
-                                &style,
-                                FontSelection::Default,
-                                Align::LEFT,
-                            );
-
-                        let is_selected = self.selected == Some(node.id);
-                        let button = Button::new(layout_job).selected(is_selected).wrap();
-
-                        if ui.add(button).clicked() {
-                            self.selected = Some(node.id);
-                        }
-
-                        ui.add_space(PADDING);
-                    }
-                });
-
-                ui.scope(|ui| {
-                    ui.set_min_height(available_height);
-                    ui.add(Separator::default().vertical());
-                });
-            });
-        });
-    }
-}
-
-impl App for Kiro<'_> {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        CentralPanel::default().show(ctx, |ui| {
-            self.render_nodes(ui);
-        });
-    }
-}
 
 fn main() -> eframe::Result {
     let mut existing_ids = HashSet::new();
